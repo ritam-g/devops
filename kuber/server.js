cf06ce43caf86bcc885caed0a4fa0c34563450e6
ritam-g <ritammaty2006@gmail.com>
2026-05-08 12:28:16 +0530

    feat: add morgan dependency and update server response to include computation result

--- kuber/server.js
+++ kuber/server.js
@@ -3,7 +3,12 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let sum = 0
+  for (let i = 0; i < 100000000; i++) {
+    // Simulate a long-running task
+    sum += i
+  }
+  return res.status(200).json({ message: 'Hello, World!', sum })
 })
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
